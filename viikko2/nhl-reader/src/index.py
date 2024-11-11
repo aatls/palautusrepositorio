1f54bb5d90befe92219598e25ff099f9f1eff16c
aatls <aatos.latva-somppi@helsinki.fi>
2024-11-11 23:53:24 +0200

    rm cmnt

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,24 +1,3 @@
-# import requests
-# from player import Player
-
-# def main():
-#     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-#     response = requests.get(url).json()
-
-#     print("JSON-muotoinen vastaus:")
-#     print(response)
-
-#     players = []
-
-#     for player_dict in response:
-#         player = Player(player_dict)
-#         players.append(player)
-
-#     print("Oliot:")
-
-#     for player in players:
-#         print(player)
-
 from player_reader import PlayerReader
 from player_stats import PlayerStats
 
